d2a09a5e33daa56b80a979b3edcd39f92d2f86ac
Hodaka-A <anxisuigao@gmail.com>
2025-10-30 21:00:34 +0900

    fix:環境変数の設定

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,7 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   output: "export",
+  assetPrefix: '/security-video',
 };
 
 export default nextConfig;
